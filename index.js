909cb4f6a73bfbbf9cf87f6df418ba82f1972aee
fosong98 <fosong98@gmail.com>
2022-03-31 12:27:42 +0900

    Make allChoose and allRelease

--- index.js
+++ index.js
@@ -21,9 +21,17 @@ function readExcel() {
     workBook.SheetNames.forEach(function (sheetName) {
       console.log('SheetName: ' + sheetName);
       let rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
-
-      for (let step = 0; step < rows.length - 1; step++) {
-        localStorage.setItem(step, JSON.stringify(rows[step+1]))
+      for (let step = 1; step < rows.length; step++) {
+        var map = new Map();
+        map.set(rows[0].__EMPTY, rows[step].__EMPTY);
+        map.set(rows[0].__EMPTY_1, rows[step].__EMPTY_1);
+        map.set(rows[0].__EMPTY_2, rows[step].__EMPTY_2);
+        map.set(rows[0].__EMPTY_3, rows[step].__EMPTY_3);
+        map.set(rows[0].__EMPTY_4, rows[step].__EMPTY_4);
+        map.set(rows[0].__EMPTY_5, rows[step].__EMPTY_5);
+        const obj = Object.fromEntries(map);
+      
+        localStorage.setItem(step, JSON.stringify(obj))
       }
       document.getElementById("upload").innerText = "업로드 완료";
     })
--- practice.js
+++ practice.js
@@ -3,5 +3,61 @@ window.addEventListener("load", start, false);
 function start() {
   setTimeout(()=>{
   document.body.setAttribute("class", "reveal");},
-  1000);
+  500);
+
+  var plants = document.getElementById("plants");
+
+  for (var i = 1; i <= localStorage.length; ++i) {
+    var plant = document.createElement("div");
+    plant.id = i.toString();
+    plant.className = "plant bg-primary";
+    var jsonString = localStorage.getItem(i);
+    plant.setAttribute("value", jsonString);
+    plant.setAttribute("onclick", "clicked()");
+    plant.innerText = JSON.parse(jsonString).Cultivar;
+    plants.append(plant);
+  }
+}
+
+function clicked() {
+  var plant = event.target;
+  if (plant.classList.contains("bg-primary")) {
+    plant.classList.remove("bg-primary");
+    plant.classList.add("bg-success");
+  } else {
+    plant.classList.add("bg-primary");
+    plant.classList.remove("bg-success");
+  }
+}
+
+function chooseAll() {
+  var choosed = document.getElementsByClassName("bg-primary");
+  
+  for (var i = 0; i < choosed.length;++i) { 
+    choosed.item(i).classList.add("bg-success");
+  }
+  
+  choosed = document.getElementsByClassName("bg-success");
+
+  for (var i = 0; i < choosed.length;++i) { 
+    var t = choosed.item(i); 
+    if (t.classList.contains("bg-primary"))
+      t.classList.remove("bg-primary");
+  }
+}
+
+function releaseAll() {
+  var release = document.getElementsByClassName("bg-success");
+  
+  for (var i = 0; i < release.length;++i) { 
+    release.item(i).classList.add("bg-primary");
+  }
+  
+  release = document.getElementsByClassName("bg-primary");
+
+  for (var i = 0; i < release.length;++i) { 
+    var t = release.item(i); 
+    if (t.classList.contains("bg-success"))
+      t.classList.remove("bg-success");
+  }
 }
